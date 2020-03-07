e2429cb272405aaa7862590a4ca50dabf18726a0
SirLanceathot <adam@razbrizz.co.uk>
2020-03-07 23:24:23 +0000

    + fix

--- src/main/java/net/mcreator/testagain/MCreatorCheeseSlices.java
+++ src/main/java/net/mcreator/testagain/MCreatorCheeseSlices.java
@@ -1,12 +1,18 @@
 
 package net.mcreator.testagain;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.UseAction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+
 @Elementstestagain.ModElement.Tag
 public class MCreatorCheeseSlices extends Elementstestagain.ModElement {
-
 	@ObjectHolder("testagain:cheeseslices")
 	public static final Item block = null;
-
 	public MCreatorCheeseSlices(Elementstestagain instance) {
 		super(instance, 2);
 	}
@@ -15,13 +21,10 @@ public class MCreatorCheeseSlices extends Elementstestagain.ModElement {
 	public void initElements() {
 		elements.items.add(() -> new FoodItemCustom());
 	}
-
 	public static class FoodItemCustom extends Item {
-
 		public FoodItemCustom() {
-			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(16).food((new Food.Builder()).hunger(1).saturation(0.4f).setAlwaysEdible()
-
-					.build()));
+			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(16)
+					.food((new Food.Builder()).hunger(1).saturation(0.4f).setAlwaysEdible().build()));
 			setRegistryName("cheeseslices");
 		}
 
@@ -34,7 +37,5 @@ public class MCreatorCheeseSlices extends Elementstestagain.ModElement {
 		public UseAction getUseAction(ItemStack par1ItemStack) {
 			return UseAction.EAT;
 		}
-
 	}
-
 }
