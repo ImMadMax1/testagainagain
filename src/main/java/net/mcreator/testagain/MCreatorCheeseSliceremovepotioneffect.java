854ea9b70a239377df1a2fa8c17471faba9213af
SirLanceathot <adam@razbrizz.co.uk>
2020-03-07 23:55:19 +0000

    - Kinda Quirkey Doe block

--- src/main/java/net/mcreator/testagain/MCreatorCheeseSliceremovepotioneffect.java
+++ src/main/java/net/mcreator/testagain/MCreatorCheeseSliceremovepotioneffect.java
@@ -1,11 +1,12 @@
 package net.mcreator.testagain;
 
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
 @Elementstestagain.ModElement.Tag
 public class MCreatorCheeseSliceremovepotioneffect extends Elementstestagain.ModElement {
-
 	public MCreatorCheeseSliceremovepotioneffect(Elementstestagain instance) {
 		super(instance, 4);
-
 	}
 
 	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
@@ -13,12 +14,8 @@ public class MCreatorCheeseSliceremovepotioneffect extends Elementstestagain.Mod
 			System.err.println("Failed to load dependency entity for procedure MCreatorCheeseSliceremovepotioneffect!");
 			return;
 		}
-
 		Entity entity = (Entity) dependencies.get("entity");
-
 		if (entity instanceof LivingEntity)
 			((LivingEntity) entity).clearActivePotions();
-
 	}
-
 }
