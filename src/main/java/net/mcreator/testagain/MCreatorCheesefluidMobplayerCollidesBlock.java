5abe3881d1b119475aa3f359cce09ae0379c8a13
Max <Max@DESKTOP-QBVTU1L.Home>
2020-03-08 00:23:00 +0000

    + cheeseliquid +achievement v1

---
+++ src/main/java/net/mcreator/testagain/MCreatorCheesefluidMobplayerCollidesBlock.java
@@ -0,0 +1,37 @@
+package net.mcreator.testagain;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
+
+import java.util.Iterator;
+
+@Elementstestagain.ModElement.Tag
+public class MCreatorCheesefluidMobplayerCollidesBlock extends Elementstestagain.ModElement {
+	public MCreatorCheesefluidMobplayerCollidesBlock(Elementstestagain instance) {
+		super(instance, 5);
+	}
+
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			System.err.println("Failed to load dependency entity for procedure MCreatorCheesefluidMobplayerCollidesBlock!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if (entity instanceof ServerPlayerEntity) {
+			Advancement _adv = ((MinecraftServer) ((ServerPlayerEntity) entity).server).getAdvancementManager()
+					.getAdvancement(new ResourceLocation("testagain:cheeseswimming"));
+			AdvancementProgress _ap = ((ServerPlayerEntity) entity).getAdvancements().getProgress(_adv);
+			if (!_ap.isDone()) {
+				Iterator _iterator = _ap.getRemaningCriteria().iterator();
+				while (_iterator.hasNext()) {
+					String _criterion = (String) _iterator.next();
+					((ServerPlayerEntity) entity).getAdvancements().grantCriterion(_adv, _criterion);
+				}
+			}
+		}
+	}
+}
